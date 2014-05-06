7857160a3053a784377da6710db0f5ff3f8e5684
Alex Lewis <alexmichael.lewis@gmail.com>
2014-05-06 01:50:09 -0400

    began changing play.js into angular to function better with the rest of the app.

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,8 @@
 angular.module('jukeroxApp')
     .controller('mainCtrl', ['$scope', '$rootScope', function($scope, $rootScope){
+
+            /* Event handler plugin, credit Jim Poulakos https://github.com/jimpoulakos */
+
             $rootScope.events = [
                 {receive: 'artist.genre.select', broadcast: 'genre.selected'}
             ];
@@ -59,6 +62,69 @@ angular.module('jukeroxApp')
             {label: 'Tracks', url: 'template/tracks.html'},
             {label: 'Genre', url: 'template/genre.html'}
         ]
+
+        $scope.$on('request.switch.genre', function(event, params){
+            $scope.template.url = 'template/genre.html';
+        });
+
+
+        var myAudio = angular.element(document.querySelector('myAudio'));
+
+        $scope.currently_playing = {
+            src: '',
+            audio: angular.element(document.querySelector('myAudio')),
+            cover: '',
+            title: '',
+            artist: '',
+            albumName: '',
+            width: 0,
+            next: function(){
+                var len = this.album.tracks.length;
+                this.queue++;
+                if(this.queue >= len) this.queue = 0;
+
+                this.select_track();
+            },
+            prev: function(){
+                this.queue--;
+                if(this.queue < 0) this.queue = this.album.tracks.length - 1;
+
+                this.select_track();
+            },
+            stop: function(){
+                this.audio.pause();
+            },
+            play: function(){
+                if(this.src != ''){
+                    this.audio.play();
+                }
+            },
+            album: {},
+            queue: 0,
+        select_new_album: function(album){
+            this.album = album;
+            this.queue = 0;
+            this.select_track();
+        },
+        select_track: function(){
+            this.cover = this.album.cover;
+            this.albumName = this.album.name;
+            this.artist = this.album.artist;
+            this.src = this.album.tracks[this.queue].src;
+            this.title = this.album.tracks[this.queue].title;
+        },
+        update: function(){
+            // Interval function to update width;
+        }
+    }
+
+        $scope.$on('play.album', function(event, params){
+            $scope.currently_playing.select_new_album(params);
+        })
+        $scope.$on('play.track', function (event, params){
+
+        })
+
     }])
         .controller('ArtistCtrl', ['$scope', 'ArtistsService', function($scope, ArtistsService){
             $scope.artists = ArtistsService.query({}, function(d){}, function(d){});
@@ -66,7 +132,11 @@ angular.module('jukeroxApp')
         .controller('AlbumCtrl', ['$scope', 'AlbumsService', function($scope, AlbumsService){
             $scope.albums = AlbumsService.query({}, function(d){}, function(d){});
             $scope.genre_clicked = function(genre){
+                $scope.$emit('request.switch.genre', {});
                 $scope.$emit('artist.genre.select', {genre: genre});
+            }
+            $scope.play_album = function(album){
+                $scope.$emit('play.album', album);
             }
         }])
         .controller('TrackCtrl', ['$scope', 'TrackService', function($scope, TrackService){
